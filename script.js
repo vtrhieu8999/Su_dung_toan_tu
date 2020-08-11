64c1bb177ed4f47de22f3952b3e58050787f51fb
Vuong_Trung_Hieu <v.hieu1998@gmail.com>
2020-08-11 08:49:03 +0700

    su dung toan tu

---
+++ script.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let width = parseFloat(inputWidth);
+let height = parseFloat(inputHeight);
+let area = width*height;
+document.write("The area is: " + area);
